4f5e6e4f33f73dca81f6d7e9a211d8c7d3b365f2
saipod <enrileharu@gmail.com>
2026-04-25 19:32:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function openModal(type) {
   modal.style.display = "block";
 
   if (type === "proof") {
-    widget.src = "https://discord.com/widget?id=YOUR_SERVER_ID&theme=dark";
-    invite.href = "https://discord.gg/YOUR_INVITE";
+    widget.src = "https://discord.com/widget?id=1441336765377024054&theme=dark";
+    invite.href = "https://discord.gg/https://discord.gg/KSq34uw6u";
   } else {
-    widget.src = "https://discord.com/widget?id=YOUR_SERVER_ID&theme=dark";
-    invite.href = "https://discord.gg/YOUR_INVITE";
+    widget.src = "https://discord.com/widget?id=1442698990054543474&theme=dark";
+    invite.href = "https://discord.gg/https://discord.gg/z4mJZAnh";
   }
 }
 
